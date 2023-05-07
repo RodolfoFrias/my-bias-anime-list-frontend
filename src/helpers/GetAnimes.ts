a8dab56e1a13d32858b8051a9ed9b8466ac82c67
Rodolfo Frías <rodofriaslugo@gmail.com>
2023-05-07 16:59:21 -0600

    feat: point to backend service intead of third API service directly

--- src/helpers/GetAnimes.ts
+++ src/helpers/GetAnimes.ts
@@ -4,28 +4,14 @@ import type { AxiosResponse } from 'axios';
 import type { Anime } from '../models/AnimeModel';
 
 const API_URL: string = import.meta.env.VITE_API_URL ?? ''; 
-const API_KEY: string = import.meta.env.VITE_RAPID_API_KEY ?? '';
-const API_HOST: string = import.meta.env.VITE_RAPID_API_HOST ?? '';
-
 
 export default async function getAnimes(): Promise<Anime[]> {
     const animesResponse = ref<Anime[]>([])
     let axiosResponse: AxiosResponse;
 
     try {
-        axiosResponse = await axios.get(API_URL, {
-            params: {
-                fields: 'id,title,main_picture,synopsis,status',
-                limit: '10'
-            },
-            headers: {
-                'X-RapidAPI-Key': API_KEY,
-                'X-RapidAPI-Host': API_HOST
-            }
-        }) 
-
+        axiosResponse = await axios.get(API_URL) 
         animesResponse.value = axiosResponse.data.animes
-
     } catch (error) {
         console.error(error)
     }
